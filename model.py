e98f51ec35d6a47e921712ead6ecab84bbf54e89
zhangjs <zhang_jinshuo@outlook.com>
2019-10-31 13:02:50 +0800

    trivial changes to model.py

--- model.py
+++ model.py
@@ -1,18 +1,46 @@
 import torch as th
 from torch import nn
+import torch.nn.functional as F
 import torchvision
 from resnet import resnet50
+from collections import OrderedDict
+
+
+def conv(cfg):
+    return nn.Sequential(
+        nn.Conv2d(cfg[0],cfg[1],cfg[2],cfg[3],padding=cfg[4],bias=False),
+        nn.BatchNorm2d(cfg[5]),
+        nn.ReLU()
+    )
+
+
+def deconv(cfg):
+    return nn.Sequential(
+        nn.ConvTranspose2d(cfg[0],cfg[1],cfg[2],cfg[3],padding=cfg[4],bias=False),
+        nn.BatchNorm2d(cfg[5]),
+        nn.ReLU()
+    )
+
+
+def features(cfg):
+    if cfg == 'resnet':
+        return resnet50(no_top=True)
+    else:
+        return nn.Sequential(OrderedDict())
 
 
 class Features(nn.Module):
     def __init__(self, config):
         super(Features, self).__init__()
 
-        self.resnet = resnet50(no_top=True)
-        self.inputs = nn.Conv2d()
+        self.inputs = self.conv(config['inputs'])
+        self.features = self.features(config['base'])
 
-    def conv(self, cfg):
-        pass
 
-    def deconv(self, cfg):
-        pass
+    def forward(self,x):
+        x=self.inputs(x)
+        x=self.features(x)
+        return x
+
+
+

47217ccf298af66a23a30adc92d36d91f2974c64
zhangjs <zhang_jinshuo@outlook.com>
2019-11-07 16:03:45 +0800

    a pdfnet is born

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch as th
 from torch import nn
 import torch.nn.functional as F
 import torchvision
-from resnet import resnet50
+from resnet import BasicBlock,Bottleneck,make_layer
 from collections import OrderedDict
 
 
@@ -13,6 +13,56 @@ from collections import OrderedDict
 #         nn.BatchNorm2d(planes),
 #         nn.ReLU(inplace=True)
 #     ) 
+
+
+
+
+# class BasicBlock(nn.Module):
+#     expansion = 1
+
+#     def __init__(self, inplanes, planes, stride=1, downsample=None):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = conv(inplanes, planes, stride)
+#         self.conv2 = conv(planes, planes, stride=1)
+#         self.downsample = downsample
+#         self.stride = stride
+
+#     def forward(self, x):
+#         residual = x
+
+#         out = self.conv1(x)
+#         out = self.conv2(out)
+
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
+
+#         out += residual
+#         out = F.relu(out)
+#         return out
+
+
+# def make_layer(inplanes, block, planes, blocks, stride=1):
+#     downsample = None
+#     if stride != 1 or inplanes != planes * block.expansion:
+#         downsample = nn.Sequential(
+#             nn.Conv2d(inplanes, planes * block.expansion,
+#                       kernel_size=1, stride=stride, bias=False),
+#             nn.BatchNorm2d(planes * block.expansion),
+#         )
+
+#     layers = []
+#     layers.append(block(inplanes, planes, stride, downsample))
+#     inplanes = planes * block.expansion
+#     for i in range(1, blocks):
+#         layers.append(block(inplanes, planes))
+
+#     return nn.Sequential(*layers)
+
+## End of the Resblock
+
+
+
+## main model
 
 def conv(in_planes, out_planes, k=3, stride=2, padding=1):
     return nn.Sequential(
@@ -32,95 +82,57 @@ def deconv(inplane, outplane):
         nn.ReLU()
     )
 
+
 def cat(x):
     return th.cat(x,-3)
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv(inplanes, planes, stride)
-        self.conv2 = conv(planes, planes, stride=1)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.conv2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = F.relu(out)
-        return out
-
-
-def make_layer(inplanes, block, planes, blocks, stride=1):
-    downsample = None
-    if stride != 1 or inplanes != planes * block.expansion:
-        downsample = nn.Sequential(
-            nn.Conv2d(inplanes, planes * block.expansion,
-                      kernel_size=1, stride=stride, bias=False),
-            nn.BatchNorm2d(planes * block.expansion),
-        )
-
-    layers = []
-    layers.append(block(inplanes, planes, stride, downsample))
-    inplanes = planes * block.expansion
-    for i in range(1, blocks):
-        layers.append(block(inplanes, planes))
-
-    return nn.Sequential(*layers)
-
-## End of the Resblock
-
-
-## main model
-
-
-
-# def features(cfg):
-#     if cfg == 'resnet':
-#         return resnet50(no_top=True)
-#     else:
-#         return nn.Sequential(OrderedDict())
-
-class Features(nn.Module):
+class PDF(nn.Module):
     def __init__(self, config):
 
-        super(Features, self).__init__()
+        super(Features, self).__ init__()
         self.inplane=inplane
         self.inputs = nn.Conv2d(6, 3,kernel_size=7,stride=2,padding=3,bias=False)
 
-        conv_planes = [32, 64, 128, 256, 512, 512, 512]
-        deconv_planes = [512, 512, 256, 128, 64, 32]
+        conv_planes = [64, 64, 128, 256, 512]
+        deconv_planes = [512, 256, 128, 64, 32, 16]
+        block_num=[3, 4, 6, 3]
 
         self.conv1 = conv(3, conv_planes[0], k=7,stride=2,padding=3)
 
+        # resblocks of 4 size
         blocks = OrderedDict([
-            ("feature_{}".format(i+2), 
-                make_layer(conv_planes[i],BasicBlock, conv_planes[i+1],blocks=2,stride=2)
-            ) for i in range(6)
+            ("layer_{}".format(i+1), 
+                make_layer(conv_planes[i],BasicBlock, conv_planes[i+1],blocks=block_num[i],stride=2)
+            ) for i in range(4)
         ])
 
         self.resblocks=nn.Sequential(blocks)
 
-        deconvs=OrderedDict([
-            ("deconv_{}".format(i+1),deconv(deconv_planes[i],deconv_planes[i+1])) for i in range(5)
+        # deconv 5 times
+        upconv=OrderedDict([
+            ("upconv_{}".format(i+1),deconv(deconv_planes[i]*2,deconv_planes[i+1])) for i in range(5)
         ])
 
-        self.deconvs=nn.Sequential(deconvs)
+        self.upsample=nn.Sequential(upsample)
 
-        self.output1=deconv(deconv_planes[1]*2,1)
-        self.output2=deconv(deconv_planes[2]*2,1)
-        self.output3=deconv(deconv_planes[3]*2,1)
-        self.output4=deconv(deconv_planes[4]*2,1)
-        self.output5=deconv(deconv_planes[5]*2,1)
+        self.conv1x1_1=conv(512*2,512,k=1,stride=1,padding=0)
+        self.conv1x1_2=conv(256*2,256,k=1,stride=1,padding=0)
+        self.conv1x1_3=conv(128*2,128,k=1,stride=1,padding=0)
+        self.conv1x1_4=conv(64*2,64,k=1,stride=1,padding=0)
+        self.conv1x1_5=conv(64+32,32,k=1,stride=1,padding=0)
+
+        # for depth
+        self.another_conv=nn.Sequential(
+            conv(512,512,stride=1),
+            conv(512,512,stride=1),
+            conv(512,512,stride=1),
+        )
+
+        self.output1=deconv(deconv_planes[2],1)
+        self.output2=deconv(deconv_planes[3],1)
+        self.output3=deconv(deconv_planes[4],1)
+        self.output4=deconv(deconv_planes[5],1)
         
         self.pose_estmation=nn.Sequential(
             conv(512, 512),
@@ -139,50 +151,79 @@ class Features(nn.Module):
 
     def forward(self,x):
         x,y=x
-        input1=self.conv1(x)
-        input2=self.conv1(y)
-        x=cat([input1,input2])
+        input1=self.conv1(x) # 32
+        input2=self.conv1(y) # 32
+        x=cat([input1,input2]) # 64
 
-        x1=self.resblocks.conv_1(x)
-        x2=self.resblocks.conv_2(x1)
-        x3=self.resblocks.conv_3(x2)
-        x4=self.resblocks.conv_4(x3)
-        x5=self.resblocks.conv_5(x4)
-        x6=self.resblocks.conv_6(x5)
+        x1=self.resblocks.layer_1(x)
+        x2=self.resblocks.layer_2(x1)
+        x3=self.resblocks.layer_3(x2)
+        x4=self.resblocks.layer_4(x3)
 
-        feature=F.max_pool2d(kernel_size=2,stride=2)
+        # feature=F.max_pool2d(kernel_size=2,stride=2)
 
         # Depth Part
-        d1=self.deconvs.deconv_1(feature)
-        d2=self.deconvs.deconv_2(cat([d1,x4]))
-        d3=self.deconvs.deconv_3(cat([d1,x3]))
-        d4=self.deconvs.deconv_4(cat([d1,x2]))
-        d5=self.deconvs.deconv_5(cat([d1,x1]))
+        d = self.another_conv(x4)
 
-        d_1_out = self.output1(d1)
-        d_2_out = self.output1(d2)
-        d_3_out = self.output1(d3)
-        d_4_out = self.output1(d4)
-        d_5_out = self.output1(d5)
+        d = cat([d,x4]) 
+        d = self.conv1x1_1(d)
+        d =self.deconvs.deconv_1(d) # 512->256
+        # d1 = self.output1(d1)
+
+        d = cat([d,x3])
+        d = self.conv1x1_1(d)
+        d =self.deconvs.deconv_2(d) # 256->128
+        d2 = self.output2(d)
+
+        d = cat([d,x2])
+        d = self.conv1x1_1(d)
+        d =self.deconvs.deconv_2(cat([d1,x4]))# 128->64
+        d3 = self.output3(d)
+
+        d = cat([d,x1])
+        d = self.conv1x1_1(d)
+        d =self.deconvs.deconv_3(cat([d1,x3]))# 64->32
+        d4 = self.output4(d)
+
+        d = cat([d,x])
+        d = self.conv1x1_1(d)
+        d =self.deconvs.deconv_4(cat([d1,x2]))# 32->16
+        d5 = self.output5(d)      
        
         # Pose Part
         p = self.pose_estmation(feature)
         p = th.flatten(p,start_dim=1)
-        p = self.fc()
+        p = self.fc(p)
 
         # Flow Part
-        f_1=self.deconvs.deconv_1(feature)
-        f_1_out = self.output1(f1)
-        f_2=self.deconvs.deconv_2(cat([d1,x4,f_1_out]))
-        f_2_out = self.output1(f2)
-        f_3=self.deconvs.deconv_3(cat([d1,x3,f_1_out]))
-        f_3_out = self.output1(f3)
-        f_4=self.deconvs.deconv_4(cat([d1,x2,f_1_out]))
-        f_4_out = self.output1(f4)
-        f_5=self.deconvs.deconv_5(cat([d1,x1,f_1_out]))
-        f_5_out = self.output1(f5)
+        f = self.another_conv(x4)
 
-        return d_5_out,p,f_5_out
+        f = cat([f,x4]) 
+        f = self.conv1x1_1(f)
+        f =self.deconvs.deconv_1(f) # 512->256
+        # f1 = self.output1(f1)
+
+        f = cat([f,x3])
+        f = self.conv1x1_2(f)
+        f =self.deconvs.deconv_2(f) # 256->128
+        f2 = self.output2(f)
+
+        f = cat([f,x2])
+        f = self.conv1x1_3(f)
+        f =self.deconvs.deconv_2(cat([d1,x4]))# 128->64
+        f3 = self.output3(f)
+
+        f = cat([f,x1])
+        f = self.conv1x1_4(f)
+        f =self.deconvs.deconv_3(cat([d1,x3]))# 64->32
+        f4 = self.output4(f)
+
+        f = cat([f,x])
+        f = self.conv1x1_5(f)
+        f =self.deconvs.deconv_4(cat([d1,x2]))# 32->16
+        f5 = self.output5(f)
+
+        return d_5,p,f_5
 
     def init_weights(self):
         # weights train from scratch
